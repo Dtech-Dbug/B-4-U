26c4ef8a8b4cc40f98fff3e3aa3309d626b0cfdf
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-06 22:04:49 +0530

    create docs : create CARD component

---
+++ hashtag/Components/Ui-Elements/CARD.js
@@ -0,0 +1,13 @@
+import styles from "../../styles/Home.module.css";
+
+function CARD() {
+  return (
+    <>
+      <div className={styles.cardContainer}>
+        <h3 className={styles.cardInfo}>Education</h3>
+      </div>
+    </>
+  );
+}
+
+export default CARD;
