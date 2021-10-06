3ccea835b0f7634ce820ea5e37d2370751eb5146
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-06 21:40:15 +0530

    created button component

---
+++ hashtag/Components/Ui-Elements/BUTTON.js
@@ -0,0 +1,12 @@
+import styles from "../../styles/Home.module.css";
+function BUTTON() {
+  return (
+    <>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.wrappedContent}>Start</button>
+      </div>
+    </>
+  );
+}
+
+export default BUTTON;
