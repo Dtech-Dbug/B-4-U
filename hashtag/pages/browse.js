01f543c87e8b8d498f5556863481f4eda209c477
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-06 22:44:24 +0530

    laying out browse page

--- hashtag/pages/browse.js
+++ hashtag/pages/browse.js
@@ -4,6 +4,10 @@ import styles from "../styles/Home.module.css";
 function browse() {
   return (
     <>
+      <header>
+        <h3 className={styles.header}>get #browsing!</h3>
+      </header>
+
       <div className={styles.cardGallery}>
         <CARD info=" Education" />
         <CARD info="Entertainment" />
