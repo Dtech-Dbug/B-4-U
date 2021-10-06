a73113b61a3abdaa6746e66a247b198875a47213
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-06 22:27:40 +0530

    renamed file

---
+++ hashtag/pages/browse.js
@@ -0,0 +1,12 @@
+import CARD from "../Components/Ui-Elements/CARD";
+
+function tf() {
+  return (
+    <>
+      <CARD info="Education" />
+      <CARD info="Entertainment" />
+    </>
+  );
+}
+
+export default tf;
