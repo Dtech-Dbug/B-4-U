efd215f6d1959ebc650d1a62c26f6db51e1ee330
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-06 21:44:15 +0530

    update index.js : linked splash screen to next screen

--- hashtag/pages/index.js
+++ hashtag/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import tf from "./tf";
+import Link from "next/link";
 
 //uis
 import BUTTON from "../Components/Ui-Elements/BUTTON";
@@ -23,7 +24,11 @@ export default function Home() {
           <header className={styles.logo}>
             <h1># hashtag</h1>
           </header>
-          <BUTTON />
+          <Link href="/tf">
+            <a href="">
+              <BUTTON />
+            </a>
+          </Link>
         </div>
       </div>
     </>
