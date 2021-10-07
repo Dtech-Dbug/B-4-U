ac1e12a4498385d047375b636989bcecb53f4c24
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-08 00:41:06 +0530

    passed formats array as props to CARD component

--- client/pages/browse.js
+++ client/pages/browse.js
@@ -1,22 +1,61 @@
 import CARD from "../Components/Ui-Elements/CARD";
 import styles from "../styles/Home.module.css";
+import { FaSearch } from "react-icons/fa";
+import { FcSearch } from "react-icons/fc";
+import { useEffect, useState } from "react";
 
 function browse() {
+  const [videoInfo, setVideoInfo] = useState([]);
+  const [videoLoaded, setVideoLoaded] = useState(false);
+  let host = "http://localhost:8000/";
+  useEffect(() => {
+    fetch(
+      host + "videoInfo?videoURL=http://www.youtube.com/watch?v=aqz-KE-bpKQ"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setVideoLoaded(false);
+        console.log("read", data);
+        console.log("link", data.videoDetails.embed.iframeUrl);
+        setVideoInfo(data);
+        setVideoLoaded(true);
+        // videoInfo.push(data);
+      })
+      .catch((err) => {
+        {
+          console.log("Error while fetching", err.message);
+        }
+      });
+  }, []);
+  const icon = () => <FaSearch />;
   return (
     <>
       <header className={styles.headerContainer}>
         <h3 className={styles.header}>get #browsing!</h3>
       </header>
-      {/* <div className={styles.cardGallery}>
-        <CARD info=" Education" />
-        <CARD info="Entertainment" />
-        <CARD info="Sports" />
-        <CARD info="Comedy" />
-      </div> */}
+
+      {/* {console.log("load state", loading)} */}
+
       <div className={styles.searchInput}>
-        <i class="fa fa-search"></i>
+        <FaSearch className={styles.searchIcon} />
         <input type="text" placeholder="Search Your State here"></input>
       </div>
+
+      <div className={styles.cardGallery}>
+        {videoLoaded && (
+          <CARD
+            data={videoInfo}
+            link={videoInfo.videoDetails.embed.iframeUrl}
+            formats={videoInfo.formats}
+          />
+        )}
+
+        {/* <CARD info="Entertainment" />
+       
+        <CARD info="Comedy" /> */}
+      </div>
+
+      {/* {icon()} */}
     </>
   );
 }
