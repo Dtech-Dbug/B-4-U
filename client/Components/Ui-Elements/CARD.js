ccf16330f00692234178485bee72cc8a3a6ad10a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-08 00:35:14 +0530

    updated : CARD

--- client/Components/Ui-Elements/CARD.js
+++ client/Components/Ui-Elements/CARD.js
@@ -2,16 +2,18 @@ import styles from "../../styles/Home.module.css";
 import ICON from "../Icons/awards.png";
 import test from "../../public/vercel.svg";
 
-function CARD({ link, title, desc }) {
+function CARD({ data, link }) {
   return (
     <>
+      {console.log("LINK", data)}
       <div className={styles.cardContainer}>
         {/* <img src="" height={150} width={150} alt="oop" /> */}
         <iframe
-          src="https://www.youtube.com/embed/aqz-KE-bpKQ"
+          className={styles.cardVideo}
+          src={link}
           frameborder="0"
         ></iframe>
-        <h3 className={styles.cardInfo}>{info}</h3>
+        <h3 className={styles.cardInfo}>h</h3>
       </div>
     </>
   );
