280bcbbdde08111ede73e3ed55f7034634e409a8
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-07 23:31:06 +0530

    added iframe to card component

--- client/Components/Ui-Elements/CARD.js
+++ client/Components/Ui-Elements/CARD.js
@@ -7,6 +7,7 @@ function CARD({ info }) {
     <>
       <div className={styles.cardContainer}>
         {/* <img src="" height={150} width={150} alt="oop" /> */}
+        <iframe src="" frameborder="0"></iframe>
         <h3 className={styles.cardInfo}>{info}</h3>
       </div>
     </>
