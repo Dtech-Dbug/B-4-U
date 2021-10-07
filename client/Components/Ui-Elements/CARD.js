1ecb1de4810e2f91eec93191e54781a745c29319
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-07 23:53:19 +0530

    update CARD : accept props

--- client/Components/Ui-Elements/CARD.js
+++ client/Components/Ui-Elements/CARD.js
@@ -2,12 +2,15 @@ import styles from "../../styles/Home.module.css";
 import ICON from "../Icons/awards.png";
 import test from "../../public/vercel.svg";
 
-function CARD({ info }) {
+function CARD({ link, title, desc }) {
   return (
     <>
       <div className={styles.cardContainer}>
         {/* <img src="" height={150} width={150} alt="oop" /> */}
-        <iframe src="" frameborder="0"></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/aqz-KE-bpKQ"
+          frameborder="0"
+        ></iframe>
         <h3 className={styles.cardInfo}>{info}</h3>
       </div>
     </>
